58f8fd0cc450b5d188bbe7c48c5757e4e0f268d4
Yegor <ef188209@gmail.com>
2025-11-22 12:30:38 +0300

    chore: init backend

---
+++ Backend/Backend/Backend/Program.cs
@@ -0,0 +1,23 @@
+namespace Backend;
+
+public abstract class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddAuthorization();
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+        app.Run();
+    }
+}
